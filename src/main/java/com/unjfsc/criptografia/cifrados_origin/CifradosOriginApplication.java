3a41e8dd4f376b296205580da0f27701d6cd974d
watercannon <0333241024@unjfsc.edu.pe>
2026-05-27 21:41:33 -0500

    Organizacion de carpetas para metodos criptograficos

---
+++ src/main/java/com/unjfsc/criptografia/cifrados_origin/CifradosOriginApplication.java
@@ -0,0 +1,13 @@
+package com.unjfsc.criptografia.cifrados_origin;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CifradosOriginApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(CifradosOriginApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/unjfsc/criptografia/cifrados_origin/CifradosOriginApplicationTests.java
@@ -0,0 +1,13 @@
+package com.unjfsc.criptografia.cifrados_origin;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CifradosOriginApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
